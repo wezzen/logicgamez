ae20274581c67d19b48eff36bd0ddfbabf573dbd
LostHikking <wiperus55@gmail.com>
2021-12-30 06:19:39 +0600

    Apply Google Java Format

--- src/main/java/com/github/wezzen/logicgamez/LogicgamezApplication.java
+++ src/main/java/com/github/wezzen/logicgamez/LogicgamezApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogicgamezApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogicgamezApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LogicgamezApplication.class, args);
+  }
 }
--- src/main/java/com/github/wezzen/logicgamez/controllers/MainController.java
+++ src/main/java/com/github/wezzen/logicgamez/controllers/MainController.java
@@ -1,19 +1,15 @@
 package com.github.wezzen.logicgamez.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class MainController {
 
-    @GetMapping
-    @RequestMapping(value = "/test")
-    public String getTestData() {
-        return "index";
-    }
-
+  @GetMapping
+  @RequestMapping(value = "/")
+  public String getMainPage() {
+    return "index";
+  }
 }
