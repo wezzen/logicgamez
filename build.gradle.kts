349d9cb2aba4c153dc18466524abb791e0020537
LostHikking <wiperus55@gmail.com>
2022-01-15 14:03:42 +0600

    Bump spotless to 6.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("org.springframework.boot") version "2.7.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("org.checkerframework") version "0.6.5"
-	id("com.diffplug.spotless") version "6.1.0"
+	id("com.diffplug.spotless") version "6.2.0"
 	java
 }
 
